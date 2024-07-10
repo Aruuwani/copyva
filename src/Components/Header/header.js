7a5599044c8e83d8928646d1d301e870e610256b
Aruuwani <waniaruu@gmail.com>
2024-07-10 08:44:57 +0530

    login_screens

--- src/Components/Header/header.js
+++ src/Components/Header/header.js
@@ -4,6 +4,12 @@ import Logo_web from "../../../src/assets/Copyva_logo.png";
 import UserIcon from "../../../src/assets/user_icon.png";
 import UserComponent from "../usercomponent";
 import { Link ,useNavigate} from "react-router-dom";
+import { IoClose } from "react-icons/io5";
+import { IoIosArrowForward } from "react-icons/io";
+
+
+
+
 
 const Header = () => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
@@ -51,8 +57,10 @@ const Header = () => {
             {isLoggedIn ? (
               <div className="profile-container" onClick={toggleMenu}>
                 <img src={UserIcon} alt="user_icon" className="user-icon" />
-                <div><span style={{color:'white'}}>{userProfile.fullName}</span></div>
-                <span style={{color:'white'}}>View profile</span>
+                <div className="profile_name"><span className="profile_name" >{userProfile.fullName}</span>
+                <span className="view_profile" >View profile</span>
+                </div>
+                
               </div>
             ) : (
               <button type="button" onClick={() => loginHandler()}>Login</button>
@@ -64,22 +72,22 @@ const Header = () => {
         <div className="side-menu">
           <div className="side-menu-header">
             <h2>Your profile</h2>
-            <button className="close-button" onClick={toggleMenu}>X</button>
+            <button className="close-button" onClick={toggleMenu}><IoClose /></button>
           </div>
           <div className="side-menu-content">
-            <p>Hello</p>
-            <h3>{userProfile.fullName}</h3>
+            <div><h3>Hello <br></br>{userProfile.fullName}</h3>
             <ul>
               <li onClick={() => handleTabClick('purchases')}>
-                <Link to='/usercomponent'>Purchases</Link>
+                <Link to='/usercomponent'>Purchases <span><IoIosArrowForward /> </span>               </Link>
               </li>
               <li onClick={() => handleTabClick('favourites')}>
-                <Link to='/usercomponent'>Favourites</Link>
+                <Link to='/usercomponent'>Favourites <span><IoIosArrowForward />  </span>              </Link>
               </li>
               <li onClick={() => handleTabClick('userinfo')}>
-                <Link to='/usercomponent'>User info</Link>
+                <Link to='/usercomponent'>User info <span><IoIosArrowForward /> </span>               </Link>
               </li>
             </ul>
+            </div>
             <button className="logout-button" onClick={handleLogout}>Log out</button>
           </div>
         </div>
